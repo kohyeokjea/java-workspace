5212af32357396fa5a36155602bb4f59ff4b996c
kohyeokjea <kohj0686@naver.com>
2024-07-03 21:51:53 +0900

    웹개발 자바 4일차

---
+++ java/src/section04/JumpEx0401.java
@@ -0,0 +1,12 @@
+package section04;
+
+public class JumpEx0401 {
+    public static void main(String[] args) {
+
+        boolean isRain =true;
+
+        if(isRain){
+            System.out.println("우산 가져가라");
+        }
+    }
+}
